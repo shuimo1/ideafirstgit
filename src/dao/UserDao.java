e9fd808ba08f0bfabe16709c105361b382ef44d8
shuimo1 <guan1502351@163.com>
2017-08-22 09:49:34 +0800

    first

---
+++ src/dao/UserDao.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * Created by Administrator on 2017/8/21 0021.
+ */
+public class UserDao {
+
+}
---
+++ src/test/TestDemo.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * Created by Administrator on 2017/8/21 0021.
+ */
+public class TestDemo {
+	public static void main(String[] args) {
+		int x=0;
+		int y=x+1;
+		int sim=++x+y;
+		System.out.println(sim);
+	}
+}
